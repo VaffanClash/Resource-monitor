df2b6f8b70124c7b2e116687d105a7ffbff9c1dd
VaffanClash <52460508+VaffanClash@users.noreply.github.com>
2022-10-17 18:51:52 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,34 @@
+const systemInfo = require('systeminformation');
+
+/* -------------------------------------------- */
+const CPUTemp = document.querySelector('#cpu_temp');
+const GPUTemp = document.querySelector('#gpu_temp');
+const GPUFan = document.querySelector('#gpu_fan');
+const GPUUsage = document.querySelector('#gpu_usage');
+
+systemInfo.powerShellStart();
+
+const updateGpuTemp = () => {
+  systemInfo.graphics().then((tmp) => {
+    GPUTemp.textContent = tmp.controllers[0].temperatureGpu + '°';
+    GPUFan.textContent = tmp.controllers[0].fanSpeed + '%';
+    GPUUsage.textContent = tmp.controllers[0].utilizationGpu + '%';
+    // console.log(tmp);
+  });
+  setTimeout(updateGpuTemp, 1000);
+};
+updateGpuTemp();
+
+const updateCpuTemp = () => {
+    systemInfo.cpuTemperature().then((tmp) => {
+      CPUTemp.textContent = Math.round(tmp.main) + '°';
+      // console.log(tmp);
+    });
+    // setTimeout(updateCpuTemp, 1000);
+}
+updateCpuTemp();
+
+// systemInfo.powerShellRelease();
+
+// systemInfo.cpuTemperature().then(data => console.log(data)).catch(err => console.error(err));
+// systemInfo.graphics().then(data => console.log(data)).catch(err => console.error(err));
